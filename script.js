b01b0d6081e887fb2dd1b844364c9314ba19f810
Alex Ray <19305443+Alexr6667@users.noreply.github.com>
2022-06-28 22:33:20 +0100

    Added interval logic. More refining needed

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
+let seconds = 1;
+let minutes = 1;
+let hours = 1;
+
+const secondText = document.querySelector("#secondText");
+const minuteText = document.querySelector("#minuteText");
+const hourText = document.querySelector("#hourText");
+
 const startBtn = document.querySelector("#startBtn");
 const stopBtn = document.querySelector("#stopBtn");
 const resetBtn = document.querySelector("#resetBtn");
 
+let intervalIDSec;
+let intervalIDMin;
+let intervalIDHour;
+
 startBtn.addEventListener("click", function () {
-  console.log("Start pressed");
+  intervalIDSec = setInterval(function () {
+    secondText.innerHTML = seconds;
+    seconds++;
+
+    if (seconds === 60) {
+      seconds = 0;
+    }
+  }, 10);
+
+  console.log(intervalIDSec);
+
+  intervalIDMin = setInterval(function () {
+    minuteText.innerHTML = minutes;
+    minutes++;
+
+    if (minutes === 59) {
+      minutes = 0;
+    }
+  }, 600);
+
+  console.log(intervalIDMin);
+
+  intervalIDHour = setInterval(function () {
+    hourText.innerHTML = hours;
+    hours++;
+  }, 3600);
+
+  console.log(intervalIDHour);
 });
 
 stopBtn.addEventListener("click", function () {
-  console.log("Stop pressed");
+  clearInterval(intervalIDSec);
+  clearInterval(intervalIDMin);
+  clearInterval(intervalIDHour);
 });
 
 resetBtn.addEventListener("click", function () {
