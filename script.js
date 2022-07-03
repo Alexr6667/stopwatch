2cefa10f892f97901ec40812bd87a18113c91bfb
Alex Ray <19305443+Alexr6667@users.noreply.github.com>
2022-07-03 21:05:29 +0100

    Updated interval timer

--- script.js
+++ script.js
@@ -14,12 +14,12 @@ startBtn.addEventListener("click", function () {
   intervalID = setInterval(function () {
     seconds++;
 
-    if (seconds == 60) {
+    if (seconds === 60) {
       seconds = 0;
       minutes++;
     }
 
-    if (minutes == 60) {
+    if (minutes === 60) {
       minutes = 0;
       hours++;
     }
@@ -27,7 +27,7 @@ startBtn.addEventListener("click", function () {
     timer.innerHTML = `${hours < 10 ? "0" + hours : hours} : ${
       minutes < 10 ? "0" + minutes : minutes
     } : ${seconds < 10 ? "0" + seconds : seconds}`;
-  }, 0);
+  }, 1000);
 });
 
 // Allows user to pause the stopwatch
